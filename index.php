4127f6fa70fc6c4720cd4ef4547218e464e7037b
chad <swansonc@beckvilleisd.net>
2014-03-27 09:13:40 -0500

    changed to use Twig template library

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo "Hello World!<br>";
-echo "Added from my iMac @ work!";
+require_once './library/Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array(
+		'cache' => './tmp/cache',
+));
+$template = $twig->loadTemplate('hello.phtml');
+$params = array(
+		'name' => 'Krzysztof',
+		'friends' => array(
+				array(
+						'firstname' => 'John ssss',
+						'lastname' => 'Smith'
+				),
+				array(
+						'firstname' => 'Britney',
+						'lastname' => 'Spears'
+				),
+				array(
+						'firstname' => 'Brad',
+						'lastname' => 'Pitt'
+				)
+		)
+);
+$template->display($params);
+?>
